efd3fee062077ba32efbbefef89460a2a85685d5
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-03-26 00:14:21 -0300

    creating tests for button component

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -3,5 +3,21 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import Button from "./Button";
 
 describe("Button Component", () => {
-  
+  const mockOnClick = jest.fn();
+  const buttonText = "Buy Now";
+
+  it("renders correctly with the given text", () => {
+    render(<Button onClick={mockOnClick} text={buttonText} />);
+
+    expect(screen.getByText(buttonText)).toBeInTheDocument();
+  });
+
+  it("calls onClick when the button is clicked", () => {
+    render(<Button onClick={mockOnClick} text={buttonText} />);
+
+    const button = screen.getByText(buttonText);
+    fireEvent.click(button);
+
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+  });
 });
