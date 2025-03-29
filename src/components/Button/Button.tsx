d53c41b22163eca09109c0615724f0403f5a7d1a
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-03-29 01:14:17 -0300

    styling button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 
 const Button = ({ onClick, text }: ButtonProps) => {
   return (
-    <button className="border p-2 rounded-md" onClick={onClick}>
+    <button className="border p-2 rounded-md hover:opacity-50 cursor-pointer" onClick={onClick}>
       {text}
     </button>
   );
