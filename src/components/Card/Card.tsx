6710cf8001c55ca87a4b48ecfa7b3941fae8f32c
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-03-30 22:24:23 -0300

    feat: updated card component to addCart logic

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,14 +10,29 @@ interface CardProps {
 }
 
 const BUTTON_TEXT = "Get Product";
+const ADD_TO_CART_TEXT = "Add to Cart";
 
 const Card = ({ id, img, name, price, onClick }: CardProps) => {
+  const addToCart = () => {
+    const cart = JSON.parse(localStorage.getItem("cart") || "[]");
+    const product = { id, img, name, price };
+    const isProductInCart = cart.some((item: any) => item.id === id);
+    if (!isProductInCart) {
+      cart.push(product);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+    alert("Product added to cart");
+  };
+
   return (
-    <div key={id} className="product">
+    <div key={id} className="product border rounded-md shadow-md">
       <img src={img} alt={name} />
       <p>{name}</p>
       <p>{price}</p>
-      <Button onClick={() => onClick(id)} text={BUTTON_TEXT} />
+      <div className="flex gap-2">
+        <Button onClick={() => onClick(id)} text={BUTTON_TEXT} />
+        <Button onClick={addToCart} text={ADD_TO_CART_TEXT} />
+      </div>
     </div>
   );
 };
