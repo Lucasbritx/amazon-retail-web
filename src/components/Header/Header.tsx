5d948a2b295ada76b4319bb06ae639f195165398
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-03-30 22:24:43 -0300

    feat: updated header links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,31 @@
-'use client';
+"use client";
 
 import React from "react";
-import Button from "../Button";
 import Input from "../Input";
-
-const BUTTONS = [
-  { text: "Home", onClick: () => {} },
-  { text: "Products", onClick: () => {} },
-  { text: "Cart", onClick: () => {} },
-  { text: "Profile", onClick: () => {} },
-  { text: "Logout", onClick: () => {} },
-];
+import Link from "next/link";
 
 const Header: React.FC = () => {
+  const BUTTONS = [
+    { text: "Home", onClick: () => {}, href: "/" },
+    { text: "Products", onClick: () => {}, href: "/products" },
+    { text: "Cart", onClick: () => {}, href: "/cart" },
+    { text: "Profile", onClick: () => {}, href: "/profile" },
+    { text: "Orders", onClick: () => {}, href: "/orders" },
+    { text: "Logout", onClick: () => {}, href: "/logout" },
+  ];
+
   return (
     <div className="w-full h-16 bg-gray-800 text-white flex items-center justify-between px-4 mb-3">
       <h1 className="text-2xl">Amazon Retail</h1>
-      <Input 
-      placeholder="Search for products..."
-      className="border p-2 rounded-md w-1/3"
+      <Input
+        placeholder="Search for products..."
+        className="border p-2 rounded-md w-1/3"
       />
       <div className="flex gap-4">
         {BUTTONS.map((button) => (
-          <Button
-            key={button.text}
-            onClick={button.onClick}
-            text={button.text}
-          />
+          <Link href={button.href} key={button.text}>
+            {button.text}
+          </Link>
         ))}
       </div>
     </div>
