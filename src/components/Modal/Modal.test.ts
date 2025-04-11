17a958d6745768b772e04ca665e23699c6fc7ace
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-11 00:40:44 -0300

    deleted unused modal component

--- src/components/Modal/Modal.test.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Modal from "./Modal";
-
-describe("Modal Component", () => {
-  
-});
--- src/components/Modal/Modal.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface ModalProps {}
-
-const Modal = ({}: ModalProps) => {
-  return <div className=""></div>;
-};
-
-export default Modal;
