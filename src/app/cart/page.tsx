490577800ce4b6c8c3244475b548b8b734e98b24
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-10 23:48:06 -0300

    feat: created submit order request on cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,16 +2,22 @@
 import { useEffect, useState } from "react";
 import Card from "@/components/Card";
 import { getCart } from "../utils/getCart";
+import { useCart } from "@/context/CartContext";
 import { Product } from "@/types/Product";
+import Button from "@/components/Button";
 
 export default function Cart() {
+  const { deleteFromCart } = useCart();
   const [cart, setCart] = useState<any>([]);
 
-  const deleteFromCart = (product: Product) => {
-    const cart = JSON.parse(localStorage.getItem("cart") || "[]");
-    const updatedCart = cart.filter((item: any) => item.id !== product.id);
-    localStorage.setItem("cart", JSON.stringify(updatedCart));
-    setCart(updatedCart);
+  const submitOrder = async () => {
+    const res = await fetch("/api/orders", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(cart),
+    });
+    const data = await res.json();
+    return data;
   };
 
   useEffect(() => {
@@ -22,10 +28,10 @@ export default function Cart() {
   }, []);
 
   return (
-    <div className="container">
+    <div className="relative">
       {cart.length && (
         <div className="flex gap-2 p-4">
-          {cart?.map((product: any) => (
+          {cart?.map((product: Product) => (
             <Card
               key={product.id}
               id={product.id}
@@ -38,6 +44,13 @@ export default function Cart() {
           ))}
         </div>
       )}
+      <Button
+        onClick={() => {
+          submitOrder();
+        }}
+      >
+        Submit Order
+      </Button>
     </div>
   );
 }
