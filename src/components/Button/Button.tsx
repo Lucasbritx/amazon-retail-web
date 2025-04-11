74e75dc8c91dc623e921de96c003d2d4e5c6e1a5
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-10 23:49:21 -0300

    bug: fixed button component behavior

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  text?: string;
-}
-
-const Button = ({ text, ...props }: ButtonProps) => {
+const Button = ({ ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
     <button
       className="border p-2 rounded-md hover:opacity-50 cursor-pointer"
       {...props}
     >
-      {text}
     </button>
   );
 };
