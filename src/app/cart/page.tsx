65f1788f297a3988e0ce1c561081cb597a68f2f4
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-11 00:10:14 -0300

    using cartItems from useCart hook on cart component

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,37 +1,28 @@
 "use client";
-import { useEffect, useState } from "react";
+import React from "react";
 import Card from "@/components/Card";
-import { getCart } from "../utils/getCart";
 import { useCart } from "@/context/CartContext";
 import { Product } from "@/types/Product";
 import Button from "@/components/Button";
 
 export default function Cart() {
-  const { deleteFromCart } = useCart();
-  const [cart, setCart] = useState<any>([]);
+  const { cartItems, deleteFromCart } = useCart();
 
   const submitOrder = async () => {
     const res = await fetch("/api/orders", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(cart),
+      body: JSON.stringify(cartItems),
     });
     const data = await res.json();
     return data;
   };
 
-  useEffect(() => {
-    const newCart = getCart();
-    newCart.then((res) => {
-      setCart(res);
-    });
-  }, []);
-
   return (
     <div className="relative">
-      {cart.length && (
+      {cartItems.length && (
         <div className="flex gap-2 p-4">
-          {cart?.map((product: Product) => (
+          {cartItems?.map((product: Product) => (
             <Card
               key={product.id}
               id={product.id}
@@ -47,7 +38,7 @@ export default function Cart() {
       <Button
         className="absolute bottom-0 right-0 m-4 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 cursor-pointer"
         type="button"
-        disabled={cart.length === 0}
+        disabled={cartItems.length === 0}
         onClick={() => {
           submitOrder();
         }}
