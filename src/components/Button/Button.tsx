a78f92d0b34c845eeeceff809bc5a89ffbdde9bd
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-18 11:51:54 -0300

    fix: button classnames syntax

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import classNames from "classnames";
 
 const Button = ({
+  className = "",
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
-  const { className = "" } = props;
   const buttonClass = classNames(
     "border p-2 rounded-md hover:opacity-50 cursor-pointer",
     className
