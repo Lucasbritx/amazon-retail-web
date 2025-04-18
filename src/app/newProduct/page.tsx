04a51cead9e5518f32b50110fac1c9ebef3fe30a
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-18 13:23:10 -0300

    creating new product route

---
+++ src/app/newProduct/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Card from "@/components/Card";
+import { Product } from "@/types/Product";
+
+export default function NewProduct() {
+
+
+  return (
+    <div className="relative">
+      <h2>Create a new product</h2>
+
+    </div>
+  );
+}
