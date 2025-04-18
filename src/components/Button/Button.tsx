2c4c2438657668f112b0412eaa8b92e3bfb21f77
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-18 11:41:04 -0300

    feat: adding aditional classnames to button component using classnames lib

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import classNames from "classnames";
 
-const Button = ({ ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
-  return (
-    <button
-      className="border p-2 rounded-md hover:opacity-50 cursor-pointer"
-      {...props}
-    >
-    </button>
+const Button = ({
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  const { className = "" } = props;
+  const buttonClass = classNames(
+    "border p-2 rounded-md hover:opacity-50 cursor-pointer",
+    className
   );
+
+  return <button className={buttonClass} {...props}></button>;
 };
 
 export default Button;
