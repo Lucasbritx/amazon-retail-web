64b257ee963bd35be61bb101e538559a20e54f82
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-20 14:11:40 -0300

    adding label to input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 type InputProps = React.ComponentProps<"input"> & {
+  label?: string;
 };
 
 const Input = ({ ...props }: InputProps) => {
   return (
-    <input
-      className="border p-2 rounded-md"
-      {...props}
-    ></input>
+    <div className="flex flex-col mb-4">
+      {props.label && (
+        <label className="text-sm text-gray-700 mb-1">{props.label}</label>
+      )}
+      <input className="border p-2 rounded-md" {...props}></input>
+    </div>
   );
 };
 
