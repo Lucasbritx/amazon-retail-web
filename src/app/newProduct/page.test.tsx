fea495822f68e29d954426994c5a53c022b3d95a
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-22 00:02:14 -0300

    feat: newProduct errors validation test

--- src/app/newProduct/page.test.tsx
+++ src/app/newProduct/page.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import NewProduct from "./page";
 
 describe("Card Component", () => {
@@ -9,5 +9,24 @@ describe("Card Component", () => {
     expect(screen.getByLabelText(/image url/i)).toHaveValue(
       "https://m.media-amazon.com/images/G/32/kindle/journeys/mdTfy5FzV17nneXV/NDQyODI5YWQt._CB545036651_.jpg"
     );
+  });
+  it("shows validation errors when fields are empty", async () => {
+    render(<NewProduct />);
+
+    fireEvent.change(screen.getByLabelText(/name/i), { target: { value: "" } });
+    fireEvent.change(screen.getByLabelText(/price/i), {
+      target: { value: "" },
+    });
+    fireEvent.change(screen.getByLabelText(/image url/i), {
+      target: { value: "" },
+    });
+
+    fireEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+    await waitFor(() => {
+      expect(screen.getByText(/Name is required/i)).toBeInTheDocument();
+      expect(screen.getByText(/Price is required/i)).toBeInTheDocument();
+      expect(screen.getByText(/Image is required/i)).toBeInTheDocument();
+    });
   });
 });
