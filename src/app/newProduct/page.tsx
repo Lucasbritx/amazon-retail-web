c2956a08891eb192b5e470f01645aa1e9884d373
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-04-23 23:23:36 -0300

    adjusting new product component

--- src/app/newProduct/page.tsx
+++ src/app/newProduct/page.tsx
@@ -5,6 +5,8 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Input from "@/components/Input";
+import { useToastContext } from "@/components/Toast/ToastContext";
+import { TOAST_TYPES } from "@/constants/TOAST_TYPES";
 
 const Product = z.object({
   name: z
@@ -24,6 +26,7 @@ const Product = z.object({
 type createProductFormData = z.infer<typeof Product>;
 
 export default function NewProduct() {
+  const { toastProps, setToastProps } = useToastContext();
   const {
     register,
     handleSubmit,
@@ -38,28 +41,43 @@ export default function NewProduct() {
   });
 
   const onSubmit: SubmitHandler<createProductFormData> = async (data) => {
-    const res = await fetch("/api/products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    if (res) {
-      const { id } = await res.json();
-      console.log("Product created with id: ", id);
-      window.location.href = "/";
-    } else {
-      console.log("Error creating product");
+    try {
+      const res = await fetch("/api/products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (res) {
+        setToastProps({
+          ...toastProps,
+          title: "Success!",
+          description: "Product created successfully!",
+          type: TOAST_TYPES.SUCCESS,
+          isOpen: true,
+        });
+        console.log("Product created successfully!");
+        setTimeout(() => {
+          window.location.href = "/";
+        }, 3000);
+      }
+    } catch (error) {
+      setToastProps({
+        ...toastProps,
+        title: "Error!",
+        description: "Error creating product",
+        type: TOAST_TYPES.ERROR,
+        isOpen: true,
+      });
+      console.log("Error creating product", error);
     }
   };
 
   return (
     <form className="flex flex-col p-4 gap-2" onSubmit={handleSubmit(onSubmit)}>
       <Input label="Name:" {...register("name")} />
-      {errors.name && (
-        <span className="text-red-500">Name is required</span>
-      )}
+      {errors.name && <span className="text-red-500">Name is required</span>}
       <Input label="Price:" type="number" {...register("price")} />
       {errors.price && <span className="text-red-500">Price is required</span>}
       <Input label="Image url" {...register("img")} />
