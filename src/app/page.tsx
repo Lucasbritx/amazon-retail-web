bc39ab275121e182612574f34ca17c17aa0da17e
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-07-10 07:26:07 -0300

    feat: updated rollup ds version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 import Card from "@/components/Card";
 import { useCart } from "@/context/CartContext";
 import { getProducts } from "./service/getProducts";
-import Button from "@/components/Button";
-import { Input, Modal } from "rollup-ds-poc";
-import "rollup-ds-poc/dist/index.css";
+import { Input, Modal, Button } from "rollup-ds-poc";
+import "rollup-ds-poc/styles";
 
 export default function Home() {
   const { addToCart } = useCart();
 
   const [products, setProducts] = useState<any>([]);
+  const [isOpen, setIsOpen] = useState(true);
 
   useEffect(() => {
     getProducts().then((res) => {
@@ -20,8 +20,17 @@ export default function Home() {
 
   return (
     <div className="">
+      <Button
+        title="Open Modal"
+        className="bg-white hover:bg-gray-200 hover:opacity-100"
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
+        Open Modal
+      </Button>
       <Input placeholder="Input from rollup DS"/>
-      <Modal isOpen={true} onClose={() => {}}>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <div className="p-4">
           <h2 className="text-lg font-bold">Welcome to the Product Page</h2>
           <p className="mt-2">Here you can find a variety of products to add to your cart.</p>
@@ -43,6 +52,7 @@ export default function Home() {
         </div>
       )}
       <Button
+        title="Add New Product"
         className="fixed bottom-0 right-0 m-4 bg-white hover:bg-gray-200 hover:opacity-100"
         onClick={() => {
           window.location.href = "/newProduct";
